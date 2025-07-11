623b0ce0d00d00574142dcb516dd0ef1ecc74ff4
Sandro Reis <consultorsandro@hotmail.com>
2025-07-11 08:19:10 -0300

    Update aap-window v.0.4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::error::Error;
+
 slint::include_modules!();
-fn main() {
-    let app = AppWindow::new().unwrap();
-    app.run().unwrap();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+
+
+
+    ui.run()?;
+
+    Ok(())
+
 }
 
